d7ac2b72c37a5e6fa374c434c975327c7f24ee9c
GuiVelMat <gvm0898@gmail.com>
2025-02-24 17:16:03 +0100

    Next. Header no aparece en /Admin

--- Next/src/components/shared/Header/Header.tsx
+++ Next/src/components/shared/Header/Header.tsx
@@ -1,90 +1,95 @@
-"use client";
+"use client"
 
-/**
- * Separamos la lógica de los datos del Header para que el componente client
- * no nos empeore el SEO. Ya que la carga de datos está en el servidor
- */
-
-import { useState, useEffect } from "react";
-import { HeaderData } from "./HeaderData";
-import React from "react";
-import { usePathname } from "next/navigation"; // Importamos useRouter
-import { useSession } from "next-auth/react";
-import { fetchWrapper } from "@/utils/fetch";
+import { useState, useEffect } from "react"
+import { HeaderData } from "./HeaderData"
+import { usePathname } from "next/navigation" // Importamos useRouter
+import { useSession } from "next-auth/react"
+import { fetchWrapper } from "@/utils/fetch"
+import React from "react"
 
 export const Header = () => {
-    const [showHeader, setShowHeader] = useState(true);
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [notifications, setNotifications] = useState<any[]>([]);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [showHeader, setShowHeader] = useState(true)
+    const [lastScrollY, setLastScrollY] = useState(0)
+    const [notifications, setNotifications] = useState<any[]>([])
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
     // Usamos useRouter para obtener la ruta activa
-    const pathname = usePathname();
+    const pathname = usePathname()
 
-    const { data: session } = useSession();
+    const { data: session } = useSession()
 
     const fetchNotifications = async () => {
         if (session) {
             try {
-                
-                const response = await fetchWrapper("/api/notifications","GET",session.user.email);
-                setNotifications(response);
+                const response = await fetchWrapper("/api/notifications", "GET", session.user.email)
+                setNotifications(response)
             } catch (error) {
-                console.error("Error al obtener las notificaciones", error);
+                console.error("Error al obtener las notificaciones", error)
             }
         }
-    };
+    }
 
     useEffect(() => {
         if (session) {
-            fetchNotifications();
+            fetchNotifications()
         }
-    }, [session]);
-
-
+    }, [session])
 
     const handleScroll = () => {
-        const currentScrollY = window.scrollY;
+        const currentScrollY = window.scrollY
         if (currentScrollY > lastScrollY && currentScrollY > 50) {
-            setShowHeader(false);
+            setShowHeader(false)
         } else {
-            setShowHeader(true);
+            setShowHeader(true)
         }
-        setLastScrollY(currentScrollY);
-    };
+        setLastScrollY(currentScrollY)
+    }
     const markAsRead = async (notificationId: number) => {
         try {
-            console.log(notificationId);
-            const response = await fetchWrapper(`/api/notifications/${notificationId}`, "PUT");
+            console.log(notificationId)
+            const response = await fetchWrapper(`/api/notifications/${notificationId}`, "PUT")
             if (response.success) {
                 setNotifications((prevNotifications) =>
                     // prevNotifications.map((notif) =>
                     //     notif.id === notificationId ? { ...notif, isRead: true } : notif
                     // )
-                    prevNotifications.filter((notif) => notif.id !== notificationId)
-                );
+                    prevNotifications.filter((notif) => notif.id !== notificationId),
+                )
             }
         } catch (error) {
-            console.error("Error al marcar la notificación como leída", error);
+            console.error("Error al marcar la notificación como leída", error)
         }
-    };
-
+    }
 
     useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll)
         return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, [lastScrollY]);
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [lastScrollY])
 
     const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
+        setIsDropdownOpen(!isDropdownOpen)
+    }
 
     return (
-        <div className={`sticky top-0 z-50 transition-transform duration-300 ${showHeader ? "translate-y-0" : "-translate-y-full"}`}>
-            {/* Le pasamos la ruta activa al componente HeaderData */}
-            <HeaderData activeRoute={pathname} session={session} notifications={notifications} isDropdownOpen={isDropdownOpen} toggleDropdown={toggleDropdown}  markAsRead={markAsRead}   />
-        </div>
-    );
-};
+        <>
+            {!pathname.toLowerCase().startsWith("/admin") ? (
+                <div
+                    className={`sticky top-0 z-50 transition-transform duration-300 ${showHeader ? "translate-y-0" : "-translate-y-full"}`}
+                >
+                    {/* Le pasamos la ruta activa al componente HeaderData */}
+                    <HeaderData
+                        activeRoute={pathname}
+                        session={session}
+                        notifications={notifications}
+                        isDropdownOpen={isDropdownOpen}
+                        toggleDropdown={toggleDropdown}
+                        markAsRead={markAsRead}
+                    />
+                </div>
+            ) : null}
+        </>
+    )
+}
+
