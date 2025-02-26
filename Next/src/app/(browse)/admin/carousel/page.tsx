381fca2d22d6c68301465544dd7460d35b79089e
GuiVelMat <gvm0898@gmail.com>
2025-02-26 18:00:36 +0100

    Next. Page carousel panel admin completa

--- Next/src/app/(browse)/admin/carousel/page.tsx
+++ Next/src/app/(browse)/admin/carousel/page.tsx
@@ -1,4 +1,5 @@
 import getCarousel from "@/actions/getCarousel";
+import { CarouselForm } from "@/components/admin/carousel/createSlideForm";
 import { CarouselAdmin } from "@/components/admin/carousel/orderCarousel";
 import React from "react";
 
@@ -6,8 +7,17 @@ const AdminCarousel = async () => {
     const slides = await getCarousel();
 
     return (
-        <div className="container mx-auto py-8">
-            <CarouselAdmin initialSlides={slides.carousels} />
+        <div className="container mx-auto py-8 px-10">
+            <div className="grid grid-cols-5 gap-10">
+                <div className="col-span-3">
+                    <h1 className="mb-4">Home carousel order selector</h1>
+                    <CarouselAdmin initialSlides={slides.carousels} />
+                </div>
+                <div className="col-span-2">
+                    <h1 className="text-end mb-4">Create new slide</h1>
+                    <CarouselForm />
+                </div>
+            </div>
         </div>
     );
 }
