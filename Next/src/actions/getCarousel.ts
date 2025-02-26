21f373dfa9aed0b188f98401f93106b9670d3a82
GuiVelMat <gvm0898@gmail.com>
2025-02-26 15:55:41 +0100

    Next. Actualizado el getCarousel para que no de problemas en redux

--- Next/src/actions/getCarousel.ts
+++ Next/src/actions/getCarousel.ts
@@ -1,16 +1,33 @@
 import { prisma } from '@/lib/prisma';
 import { CarouselResponse } from '@/types/Carousel';
-import redis from '@/lib/redis';
+// import redis from '@/lib/redis';
 
 export default async function getCarousel(): Promise<CarouselResponse> {
-  const cacheKey = 'carousel:data';
+  // const cacheKey = 'carousel:data';
 
-  const cachedData = await redis.get(cacheKey);
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
+  // const cachedData = await redis.get(cacheKey);
+  // if (cachedData) {
+  //   return JSON.parse(cachedData);
+  // }
 
-  const data = await prisma.carousel.findMany();
+  const data = await prisma.carousel.findMany({
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+      image: true,
+      href: true,
+      description: true,
+      status: true,
+      slide_number: true,
+    },
+    where: {
+      status: 'ACTIVE'
+    },
+    orderBy: {
+      slide_number: 'asc',
+    },
+  });
 
   const response: CarouselResponse = {
     carousels: data.map((carousel) => ({
@@ -18,7 +35,7 @@ export default async function getCarousel(): Promise<CarouselResponse> {
     })),
   };
 
-  await redis.set(cacheKey, JSON.stringify(response), 'EX', 3600);
+  // await redis.set(cacheKey, JSON.stringify(response), 'EX', 3600);
 
   return response;
 }
