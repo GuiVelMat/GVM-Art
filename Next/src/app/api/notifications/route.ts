d48fd2f1bbb103dd9a76a27079a30387f556d3ee
GuiVelMat <gvm0898@gmail.com>
2025-03-02 12:46:39 +0100

    Next. Api routes para POST de notificaciones globales

--- Next/src/app/api/notifications/route.ts
+++ Next/src/app/api/notifications/route.ts
@@ -1,11 +1,40 @@
 import { getNotifications } from '@/actions/getNotifications';
+import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 import { ApiResponse } from '../exceptions';
-import {prisma} from '@/lib/prisma';
-
 
 export const GET = async () => {
     const notifications = await getNotifications();
     return NextResponse.json(notifications);
 }
 
+export const POST = async (req: NextRequest) => {
+    const body = await req.json();
+
+    try {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true
+            }
+        })
+
+        if (!users.length) {
+            return ApiResponse.badRequest("No users found");
+        }
+
+        const notifications = await prisma.$transaction(
+            users.map(user => prisma.notification.create({
+                data: {
+                    message: body.message,
+                    userId: user.id,
+                    notificationType: body.notificationType
+                }
+            }))
+        )
+
+        return ApiResponse.ok(`Success, ${notifications.length} notifications sent`);
+    } catch (error) {
+        console.error(error);
+        return ApiResponse.badRequest(error);
+    }
+}
