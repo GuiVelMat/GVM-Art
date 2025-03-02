b5af40d4ffcaa5bd63aa5b5a6fd8581b8a7ec759
GuiVelMat <gvm0898@gmail.com>
2025-03-02 12:47:21 +0100

    Next. Hecha bien la card de users admin

--- Next/src/components/admin/Cards/CardUsersAdmin.tsx
+++ Next/src/components/admin/Cards/CardUsersAdmin.tsx
@@ -3,6 +3,7 @@
 import type { ProfileItem } from "@/types/Profile"
 import { UserStatusButton } from "../buttons/UserStatusButton"
 import React from "react"
+import Image from "next/image"
 
 interface CardUsersAdminProps {
     user: ProfileItem
@@ -11,10 +12,14 @@ interface CardUsersAdminProps {
 export const CardUsersAdmin = ({ user }: CardUsersAdminProps) => {
     return (
         <div className="bg-white p-4 shadow rounded">
-            <h1 className="text-lg font-semibold">
-                {user.username} <span className="text-sm text-zinc-500">({user.user.role})</span>
-            </h1>
-            <p className="text-sm text-gray-600">{user.user.email}</p>
+            <div className="flex gap-4 items-start border-separate border-b border-gray-400 pb-2">
+                <Image src={user.avatar ?? "/default-avatar.png"} alt={user.username} width={100} height={100} className="rounded-xl" />
+                <div className="flex flex-col">
+                    <h1 className="text-sm text-zinc-500">{user.user.role}</h1>
+                    <h1 className="text-lg font-semibold">{user.username}</h1>
+                    <p className="text-sm text-gray-600">{user.user.email}</p>
+                </div>
+            </div>
 
             <UserStatusButton userId={user.id} currentStatus={user.user.status} />
         </div>
