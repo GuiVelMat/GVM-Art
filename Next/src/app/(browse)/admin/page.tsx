815537d2d70eedb0fa2feaf2617179f511ada516
GuiVelMat <gvm0898@gmail.com>
2025-03-08 23:56:49 +0100

    Next. Page para las analytics del panel admin

--- Next/src/app/(browse)/admin/page.tsx
+++ Next/src/app/(browse)/admin/page.tsx
@@ -1,9 +1,40 @@
+import { getCollectionPopularity, getMonthlySales, getMostBoughtProduct, getMostLikedProduct, getSeriesPopularity, getWeeklySales } from "@/actions/getAnalytics";
+import { ChartBoughtProducts } from "@/components/admin/analytics/chart-BoughtProducts";
+import { ChartCollection } from "@/components/admin/analytics/chart-collections";
+import { ChartLikedProducts } from "@/components/admin/analytics/chart-LikedProducts";
+import { ChartSeries } from "@/components/admin/analytics/chart-series";
+import { ChartWeeklyRevenue } from "@/components/admin/analytics/chart-weeklyRevenue";
 import React from "react";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+    const productPopularity = await getMostLikedProduct();
+    const productBought = await getMostBoughtProduct();
+    const seriesPopularity = await getSeriesPopularity();
+    const collectionPopularity = await getCollectionPopularity();
+    const weeklySales = await getWeeklySales();
+
     return (
-        <div>
-            <h1>Admin Page</h1>
+        <div className="container mx-auto pt-8">
+            <div className="mb-3">
+                <h1 className="text-3xl font-bold tracking-tight">Analytics</h1>
+                <p className="text-muted-foreground mt-2">
+                    Up to date analytics from the platform products and revenue.
+                </p>
+            </div>
+
+            <div className="justify-around">
+                <div className="grid grid-cols-3 gap-4">
+                    <ChartLikedProducts products={productPopularity} />
+
+                    <ChartSeries products={seriesPopularity} />
+                    <ChartCollection products={collectionPopularity} />
+
+                    <ChartBoughtProducts products={productBought} />
+                    <div className="col-span-2">
+                        <ChartWeeklyRevenue products={weeklySales} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
